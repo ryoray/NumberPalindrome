0f5c3f626a0c57005954039c31b0705a86c7699d
ryoray <praretno.wibowo@gmail.com>
2020-04-24 07:02:34 +0700

    Add more testing in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(NumberPalindrome.isPalindrome(128));
+        System.out.println(NumberPalindrome.isPalindrome(12821));
+        System.out.println(NumberPalindrome.isPalindrome(-12821));
+        System.out.println(NumberPalindrome.isPalindrome(1287));
+        System.out.println(NumberPalindrome.isPalindrome(-1287));
     }
 }
